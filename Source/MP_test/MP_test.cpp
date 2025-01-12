d0a07a3184dbd79c6f5c51d9c05764873618726f
Vytur <juknys98@gmail.com>
2025-01-12 16:24:35 +0200

    Added steam subsystem (480)

---
+++ Source/MP_test/MP_test.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MP_test.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MP_test, "MP_test" );
